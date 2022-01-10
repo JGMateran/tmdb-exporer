c458482654c2602f76f9df3ee3e09a0fc7cdb9dc
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-01-10 01:29:15 -0400

    Correcion en el modo oscuro del boton

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -9,7 +9,7 @@ export const Button = ({
     <button
       {...props}
       className={clsx(
-        'dark:border-gray-400 focus:outline-none focus:bg-gray-800/90 active:scale-95 h-11 border px-4 rounded-md text-sm font-extrabold',
+        'dark:border-gray-500 border-gray-400 focus:outline-none dark:focus:bg-gray-800/90 focus:bg-gray-200 active:scale-95 h-11 border px-4 rounded-md text-sm uppercase font-semibold',
         className
       )}
     />
