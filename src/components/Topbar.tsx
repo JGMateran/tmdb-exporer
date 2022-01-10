5a41c88f0641b912738a012bdcc25c1bc95b2754
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-01-10 02:54:16 -0400

    He cambiado el titulo del menu principal

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -8,11 +8,11 @@ export const Topbar = () => {
   return (
     <div className="top-0 sticky z-10 bg-white/90 dark:bg-gray-900/90 text-black dark:text-white backdrop-blur-sm">
       <Container className="h-14 flex items-center">
-        <Logo className="mr-2" />
 
         <h2 className="flex-1 text-xl font-extrabold">
-          <Link to="/">
-            reburn.dev
+          <Link to="/" className="flex items-center">
+            <Logo className="mr-4" />
+            Explorador
           </Link>
         </h2>
 
