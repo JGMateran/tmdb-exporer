12d1827f90b57ff1818a1121a8d3943c7b4e9ce3
Jose Gregorio Materan <josemateran1998@gmail.com>
2022-01-12 18:41:46 -0400

    fix(Error): corrección de altura

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -7,7 +7,7 @@ const meta = {
 export const Error = () => {
   return (
     <Layout {...meta}>
-      <div className="min-h-screen flex items-center justify-center text-center">
+      <div className="min-h-[calc(100vh-90px)] pt-14 flex items-center justify-center text-center">
         <h2 className="text-5xl font-extrabold leading-normal">
           404: Pagina no encontrada
         </h2>
